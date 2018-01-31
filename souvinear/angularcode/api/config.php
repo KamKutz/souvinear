4c69c5d598e1e1ba2b7c3ecfb903d0dd2b99c105
Anisa Sloan <sloanisa@gmail.com>
2018-01-31 02:27:54 -0500

    angular week03 update

---
+++ souvinear/angularcode/api/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'sloanisa_test');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'sloanisa_UACtest');
+
+?>
