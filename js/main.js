c5bb9faf1d1ef31ae1a68ccbf9d00d9c566ba2dd
Kaity <kaitlynn281@gmail.com>
2018-02-20 18:44:14 -0500

    spotify and googlemaps together

--- js/main.js
+++ js/main.js
@@ -5,41 +5,41 @@ var mainContainer = document.getElementById('js-main-container'),
 
 var spotifyPlayer = new SpotifyPlayer();
 
-var template = function (data) {
-  return `
-    <div class="main-wrapper">       
-      <div class="now-playing__img">
-        <img src="${data.item.album.images[0].url}">
-      </div>
-      <div class="now-playing__side">
-        <div class="now-playing__name">${data.item.name}</div>
-        <div class="now-playing__artist">${data.item.artists[0].name}</div>
-        <div class="now-playing__status">${data.is_playing ? 'Playing' : 'Paused'}</div>
-        <div class="progress">
-          <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
-        </div>
-      </div>
-    </div>
-    <div class="background" style="background-image:url(${data.item.album.images[0].url})"></div>
-  `;
-};
-
-spotifyPlayer.on('update', response => {
-  mainContainer.innerHTML = template(response);
-});
-
-spotifyPlayer.on('login', user => {
-  if (user === null) {
-    loginContainer.style.display = 'block';
-    mainContainer.style.display = 'none';
-  } else {
-    loginContainer.style.display = 'none';
-    mainContainer.style.display = 'block';
-  }
-});
-
-loginButton.addEventListener('click', () => {
-    spotifyPlayer.login();
-});
-
-spotifyPlayer.init();
+//var template = function (data) {
+//  return `
+//    <div class="main-wrapper">       
+//      <div class="now-playing__img">
+//        <img src="${data.item.album.images[0].url}">
+//      </div>
+//      <div class="now-playing__side">
+//        <div class="now-playing__name">${data.item.name}</div>
+//        <div class="now-playing__artist">${data.item.artists[0].name}</div>
+//        <div class="now-playing__status">${data.is_playing ? 'Playing' : 'Paused'}</div>
+//        <div class="progress">
+//          <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
+//        </div>
+//      </div>
+//    </div>
+//    <div class="background" style="background-image:url(${data.item.album.images[0].url})"></div>
+//  `;
+//};
+//
+//spotifyPlayer.on('update', response => {
+//  mainContainer.innerHTML = template(response);
+//});
+//
+//spotifyPlayer.on('login', user => {
+//  if (user === null) {
+//    loginContainer.style.display = 'block';
+//    mainContainer.style.display = 'none';
+//  } else {
+//    loginContainer.style.display = 'none';
+//    mainContainer.style.display = 'block';
+//  }
+//});
+//
+//loginButton.addEventListener('click', () => {
+//    spotifyPlayer.login();
+//});
+//
+//spotifyPlayer.init();
