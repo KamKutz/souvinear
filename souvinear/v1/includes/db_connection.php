d72de502edc70589464115605a055cc6d4ebdfa9
Anisa Sloan <sloanisa@gmail.com>
2018-02-20 23:00:38 -0500

    security

--- souvinear/v1/includes/db_connection.php
+++ souvinear/v1/includes/db_connection.php
@@ -2,7 +2,7 @@
 
       define('DB_SERVER', 'localhost');
       define('DB_USER', 'sloanisa_svnr');
-      define('DB_PASS', 'Ncm-OT&aTe.C');
+      define('DB_PASS', '');
       define('DB_NAME', 'sloanisa_souvinear');
 
       $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
